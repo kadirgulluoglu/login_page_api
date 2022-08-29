780e7f501dfb9dd1a38095fa2f86a6c29d4f8bcf
kadirgulluoglu <62450370+kadirgulluoglu@users.noreply.github.com>
2022-08-29 13:52:09 +0300

    custom card widget

---
+++ lib/product/custom_card.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+import '../core/init/theme/theme.dart';
+
+class buildCard extends StatelessWidget {
+  const buildCard({
+    Key? key,
+    required this.title,
+    required this.subtitle,
+  }) : super(key: key);
+
+  final String title;
+  final String subtitle;
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      color: Colors.black54,
+      child: ListTile(
+        title: Text(
+          title,
+          style: Theme.of(context)
+              .textTheme
+              .headline6!
+              .copyWith(color: white, fontWeight: FontWeight.w900),
+        ),
+        subtitle: Text(
+          subtitle,
+          style: Theme.of(context).textTheme.subtitle2?.copyWith(color: white),
+        ),
+      ),
+    );
+  }
+}
